7d503fc1c2340af8f88705e45704bd8e78f5c954
Sadiq Al-Sandouk <sadiqalsandouk@hotmail.com>
2021-01-19 22:14:54 +0000

    imported libraries

---
+++ FYP.py
@@ -0,0 +1,8 @@
+#import libraries
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifer
+from PIL import Image
+import streamlit as st
+
